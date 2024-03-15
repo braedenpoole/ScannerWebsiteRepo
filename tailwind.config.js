5518f76f553a57f7e02cc44b19107feb10f5cff7
braedenpoole <braedenpoole@gmail.com>
2024-03-14 21:04:41 -0400

    Temporarily added hidden Salus Website

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+// tailwind.config.js
+
+module.exports = {
+    theme: {
+      extend: {
+        fontFamily: {
+          'playfair': ['Playfair Display', 'serif'],
+        },
+      },
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
